ce04e54352ea23e67ef47ab994a02fd960e68233
c14210056 <tiffanyjacklin29@gmail.com>
2024-06-05 15:59:09 +0700

    6/5/2024

---
+++ ui/index.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+require "connect.php";
+include "header.php"
+?>
+
+<body>
+    <div class="bg-body-index">
+        <div class="container">
+            <div class="content">
+                <div class="text">
+                    <h1 class="display-6 fst-italic">Insurance for A Worry-free Trip</h1>
+                    <p class="lead my-3">Stay protected on your travels! Enjoy most of your journey by ensuring that you and your trip are covered against unpredictable events, without the extra hassle</p>
+                </div>
+                <img src="images/chill.png" alt="Chill Image">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <span class="title-form mb-4">Learn more about our products</span>
+        <div class="container">
+            <div class="row row-cols-6">
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>
+                <div class="col">
+                    <div class="card border-0 shadow-sm mb-4">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-plane-departure fa-2x me-2" style="color: #1ba0e2;"></i>
+                            <span class="menu-title">Travel Insurance</span>
+                        </div>
+                    </div>            
+                </div>        
+            </div>
+        </div>
+    </div>
+</div>
+    
+</body>
